fc4b223a927078827f70514403c22f1a68c1dbae
Ilio Catallo <ilio.catallo@gmail.com>
2018-08-16 13:51:03 +0200

    Added Dispatcher

---
+++ test/dispatcher.test.js
@@ -0,0 +1,38 @@
+/*jshint node: true, esnext: true */
+/*global describe, before, beforeEach, it */
+'use strict';
+
+const chai          = require('chai'),
+      sinon         = require('sinon'),
+      Dispatcher    = require('../lib/dispatcher'),
+      {expect}      = chai;
+
+chai.use(require('sinon-chai'));
+
+describe('[Dispatcher] Adding a handler function', function () {
+    it('should be delegated to the underlying trie', function () {
+        const trie = {
+            setValue: sinon.stub()
+        };
+
+        const dispatcher = new Dispatcher();
+        dispatcher._trie = trie;
+
+        dispatcher.addHandler(['pred1', 'pred2'], 'handler');
+        expect(trie.setValue).to.have.been.calledWith(['pred1', 'pred2'], 'handler');
+    });
+});
+
+describe('[Dispatcher] Getting a handler function', function () {
+    it('should be delegated to the underlying trie', function () {
+        const trie = {
+            getValue: sinon.stub()
+        };
+
+        const dispatcher = new Dispatcher();
+        dispatcher._trie = trie;
+
+        dispatcher.getHandler(['arg1', 'arg2']);
+        expect(trie.getValue).to.have.been.calledWith(['arg1', 'arg2']);
+    });
+});
