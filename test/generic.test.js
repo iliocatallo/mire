21d8631c639c430631330a0ddc7c7bbc9148747f
Ilio Catallo <ilio.catallo@gmail.com>
2022-01-30 12:40:10 +0100

    Replace mocha with uvu

--- test/generic.test.js
+++ test/generic.test.js
@@ -1,161 +1,154 @@
 /* jshint node: true, esversion: 6 */
-/* global describe, it */
 'use strict';
 
-const assert   = require('assert').strict,
-      Generic  = require('../lib/generic');
+const {test}    = require('uvu'),
+      assert    = require('uvu/assert'),
+      Generic   = require('../lib/generic');
 
 const isNumber = x => Number(parseFloat(x)) === x;
 const isArray = xs => Array.isArray(xs);
 
-describe('[Generic] Creating a generic function', function () {
-    it('is possible by specifying its name, length and fallback handler', function () {
-        const sum = Generic.create({
-            name: 'sum',
-            length: 2,
-            fallback: (x, y) => x + y
-        });
-
-        assert.equal(sum.name, 'sum');
-        assert.equal(sum.length, 2);
+test('Creating a generic function is possible by specifying its name, length and fallback handler', function () {
+    const sum = Generic.create({
+        name: 'sum',
+        length: 2,
+        fallback: (x, y) => x + y
     });
 
-    it('is possible also when no name has been specified', function () {
-        const sum = Generic.create({
-            length: 2,
-            fallback: (x, y) => x + y
-        });
-
-        assert.equal(sum.name, '');
-    });
-
-    it('is possible also when no length has been specified', function () {
-        const sum = Generic.create({
-            name: 'sum',
-            fallback: (x, y) => x + y
-        });
-
-        assert.equal(sum.length, 0);
-    });
-
-    it('is possible also when no fallback handler has been specified', function () {
-        const sum = Generic.create({
-            name: 'sum',
-            length: 2
-        });
-
-        assert.throws(() => sum(5, 6), Generic.NoMatchingError);
-    });
-
-    it('is possible also when no parameters have been specified', function () {
-        const sum = Generic.create();
-
-        assert.equal(sum.name, '');
-        assert.equal(sum.length, 0);
-        assert.throws(() => sum(5, 6), Generic.NoMatchingError);
-    });
-
-    it('is not possible if the fallback handler is not a function', function () {
-        assert.throws(() => Generic.create({fallback: true}), TypeError);
-        assert.throws(() => Generic.create({fallback: 5}), TypeError);
-        assert.throws(() => Generic.create({fallback: 'string'}), TypeError);
-        assert.throws(() => Generic.create({fallback: Symbol()}), TypeError);
-        assert.throws(() => Generic.create({fallback: {a: 1, b: 2}}), TypeError);
-        assert.throws(() => Generic.create({fallback: [1, 2, 3]}), TypeError);
-    });
+    assert.equal(sum.name, 'sum');
+    assert.equal(sum.length, 2);
 });
 
-describe('[Generic] Promoting a function to a generic function', function () {
-
-    it('is possible when the argument is a function', function () {
-        const sum = Generic.of(function sum(x, y) {
-            return x + y;
-        });
-
-        assert.equal(sum.name, 'sum');
-        assert.equal(sum.length, 2);
+test('Creating a generic function is possible also when no name has been specified', function () {
+    const sum = Generic.create({
+        length: 2,
+        fallback: (x, y) => x + y
     });
 
-    it('is not possible when the argument is not a function', function () {
-        assert.throws(() => Generic.of(true), TypeError);
-        assert.throws(() => Generic.of(5), TypeError);
-        assert.throws(() => Generic.of('string'), TypeError);
-        assert.throws(() => Generic.of(Symbol()), TypeError);
-        assert.throws(() => Generic.of({a: 1, b: 2}), TypeError);
-        assert.throws(() => Generic.of([1, 2, 3]), TypeError);
-    });
+    assert.equal(sum.name, '');
 });
 
-describe('[Generic] Applying a generic function to some arguments', function () {
-    it('is delegated to a matching handler', function () {
-        const sum = Generic.create({
-            length: 2
-        });
-        sum.when([isNumber, isNumber], (x, y) => x + y);
-        sum.when([isArray, isArray], (xs, ys) => xs.map((x, i) => x + ys[i]));
-
-        const numberResult = sum(5, 6);
-        const arrayResult = sum([7, 2], [3, 4]);
-
-        assert.equal(numberResult, 11);
-        assert.deepEqual(arrayResult, [10, 6]);
+test('Creating a generic function is possible also when no length has been specified', function () {
+    const sum = Generic.create({
+        name: 'sum',
+        fallback: (x, y) => x + y
     });
 
-    it('defaults to the fallback handler when arguments do not match', function () {
-        const sum = Generic.create({
-            fallback: (x, y) => x + y
-        });
-
-        const numberResult = sum(5, 6);
-        const stringResult = sum('abcd', 'EFGH');
-
-        assert.equal(numberResult, 11);
-        assert.equal(stringResult, 'abcdEFGH');
-    });
-
-    it('accepts a variable number of arguments', function () {
-        const sum = Generic.create({
-            length: 2
-        });
-        sum.when([isNumber, isNumber], (x, y) => x + y);
-
-        const numberResult = sum(5, 6, 'extra');
-
-        assert.equal(numberResult, 11);
-    });
+    assert.equal(sum.length, 0);
 });
 
-describe('[Generic] Extending a generic function', function () {
-    it('overrides the possible handler associated with the same predicates', function () {
-        const sum = Generic.create({
-            length: 1
-        });
-
-        sum.when([isNumber], x => x + 2);
-        sum.when([isNumber], x => x + 10);
-        const numberResult = sum(5);
-
-        assert.equal(numberResult, 15);
+test('Creating a generic function is possible also when no fallback handler has been specified', function () {
+    const sum = Generic.create({
+        name: 'sum',
+        length: 2
     });
 
-    it('is not possible if the first argument is not an array of predicates of the right length', function () {
-        const sum = Generic.create({
-            length: 2
-        });
-        const handler = (x, y) => x + y;
-
-        assert.throws(() => sum.when([isNumber], handler), TypeError);
-        assert.throws(() => sum.when('not-an-array', handler), TypeError);
-        assert.throws(() => sum.when(['not-fn', 'not-fn'], handler), TypeError);
-        assert.throws(() => sum.when(['not-fn', isNumber], handler), TypeError);
-        assert.throws(() => sum.when([isNumber, 'not-fn'], handler), TypeError);
-    });
-
-    it('is not possible if the second argument is not a function', function () {
-        const sum = Generic.create({
-            length: 2
-        });
-
-        assert.throws(() => sum.when([isNumber, isNumber], 'not-a-fn'), TypeError);
-    });
+    assert.throws(() => sum(5, 6), err => err instanceof Generic.NoMatchingError);
 });
+
+test('Creating a generic function is possible also when no parameters have been specified', function () {
+    const sum = Generic.create();
+
+    assert.equal(sum.name, '');
+    assert.equal(sum.length, 0);
+    assert.throws(() => sum(5, 6), err => err instanceof Generic.NoMatchingError);
+});
+
+test('Creating a generic function is not possible if the fallback handler is not a function', function () {
+    assert.throws(() => Generic.create({fallback: true}), err => err instanceof TypeError);
+    assert.throws(() => Generic.create({fallback: 5}), err => err instanceof TypeError);
+    assert.throws(() => Generic.create({fallback: 'string'}), err => err instanceof TypeError);
+    assert.throws(() => Generic.create({fallback: Symbol()}), err => err instanceof TypeError);
+    assert.throws(() => Generic.create({fallback: {a: 1, b: 2}}), err => err instanceof TypeError);
+    assert.throws(() => Generic.create({fallback: [1, 2, 3]}), err => err instanceof TypeError);
+});
+
+test('Promoting a function to a generic function is possible when the argument is a function', function () {
+    const sum = Generic.of(function sum(x, y) {
+        return x + y;
+    });
+
+    assert.equal(sum.name, 'sum');
+    assert.equal(sum.length, 2);
+});
+
+test('Promoting a function to a generic function is not possible when the argument is not a function', function () {
+    assert.throws(() => Generic.of(true), err => err instanceof TypeError);
+    assert.throws(() => Generic.of(5), err => err instanceof TypeError);
+    assert.throws(() => Generic.of('string'), err => err instanceof TypeError);
+    assert.throws(() => Generic.of(Symbol()), err => err instanceof TypeError);
+    assert.throws(() => Generic.of({a: 1, b: 2}), err => err instanceof TypeError);
+    assert.throws(() => Generic.of([1, 2, 3]), err => err instanceof TypeError);
+});
+
+test('Applying a generic function to some arguments is delegated to a matching handler', function () {
+    const sum = Generic.create({
+        length: 2
+    });
+    sum.when([isNumber, isNumber], (x, y) => x + y);
+    sum.when([isArray, isArray], (xs, ys) => xs.map((x, i) => x + ys[i]));
+
+    const numberResult = sum(5, 6);
+    const arrayResult = sum([7, 2], [3, 4]);
+
+    assert.equal(numberResult, 11);
+    assert.equal(arrayResult, [10, 6]);
+});
+
+test('Applying a generic function to some arguments defaults to the fallback handler when arguments do not match', function () {
+    const sum = Generic.create({
+        fallback: (x, y) => x + y
+    });
+
+    const numberResult = sum(5, 6);
+    const stringResult = sum('abcd', 'EFGH');
+
+    assert.equal(numberResult, 11);
+    assert.equal(stringResult, 'abcdEFGH');
+});
+
+test('Applying a generic function to some arguments accepts a variable number of arguments', function () {
+    const sum = Generic.create({
+        length: 2
+    });
+    sum.when([isNumber, isNumber], (x, y) => x + y);
+
+    const numberResult = sum(5, 6, 'extra');
+
+    assert.equal(numberResult, 11);
+});
+
+test('Extending a generic function overrides the possible handler associated with the same predicates', function () {
+    const sum = Generic.create({
+        length: 1
+    });
+
+    sum.when([isNumber], x => x + 2);
+    sum.when([isNumber], x => x + 10);
+    const numberResult = sum(5);
+
+    assert.equal(numberResult, 15);
+});
+
+test('Extending a generic function is not possible if the first argument is not an array of predicates of the right length', function () {
+    const sum = Generic.create({
+        length: 2
+    });
+    const handler = (x, y) => x + y;
+
+    assert.throws(() => sum.when([isNumber], handler), err => err instanceof TypeError);
+    assert.throws(() => sum.when('not-an-array', handler), err => err instanceof TypeError);
+    assert.throws(() => sum.when(['not-fn', 'not-fn'], handler), err => err instanceof TypeError);
+    assert.throws(() => sum.when(['not-fn', isNumber], handler), err => err instanceof TypeError);
+    assert.throws(() => sum.when([isNumber, 'not-fn'], handler), err => err instanceof TypeError);
+});
+
+test('Extending a generic function is not possible if the second argument is not a function', function () {
+    const sum = Generic.create({
+        length: 2
+    });
+
+    assert.throws(() => sum.when([isNumber, isNumber], 'not-a-fn'), err => err instanceof TypeError);
+});
+
+test.run();
--- test/trie.test.js
+++ test/trie.test.js
@@ -1,54 +1,50 @@
 /* jshint node: true, esversion: 6 */
-/* global describe, it */
 'use strict';
 
-const assert   = require('assert').strict,
-      Trie     = require('../lib/trie');
+const {test} = require('uvu'),
+      assert = require('uvu/assert'),
+      Trie   = require('../lib/trie');
 
-describe('[Trie] Getting a value', function () {
+test('Getting a value succeeds if the value exists and the input features match', function () {
+    const trie = new Trie();
+    const pred1 = a => a === 'feature1';
+    const pred2 = b => b === 'feature2';
 
-    it('succeeds if the value exists and the input features match', function () {
-        const trie = new Trie();
-        const pred1 = a => a === 'feature1';
-        const pred2 = b => b === 'feature2';
+    trie.setValue([pred1, pred2], 'pred1-pred2-value');
 
-        trie.setValue([pred1, pred2], 'pred1-pred2-value');
-
-        assert.equal(trie.getValue(['feature1', 'feature2']), 'pred1-pred2-value');
-    });
-
-    it('results in undefined if the value does not exist', function () {
-        const trie = new Trie();
-        const pred1 = a => a === 'feature1';
-        const pred2 = b => b === 'feature2';
-
-        trie.setValue([pred1, pred2], 'pred1-pred2-value');
-
-        assert.equal(trie.getValue(['feature1']), undefined);
-        assert.equal(trie.getValue(['feature1', 'feature2', 'feature3']), undefined);
-    });
-
-    it('results in undefined if the input features are not satisfied', function () {
-        const trie = new Trie();
-        const pred1 = a => true
-        const pred2 = b => false
-
-        trie.setValue([pred1, pred2], 'pred1-pred2-value');
-
-        assert.equal(trie.getValue(['feature1', 'feature2']), undefined);
-    });
+    assert.equal(trie.getValue(['feature1', 'feature2']), 'pred1-pred2-value');
 });
 
-describe('[Trie] Setting a value', function () {
+test('Getting a value results in undefined if the value does not exist', function () {
+    const trie = new Trie();
+    const pred1 = a => a === 'feature1';
+    const pred2 = b => b === 'feature2';
 
-    it('overrides previous values associated with the same predicates', function () {
-        const trie = new Trie();
-        const pred1 = a => a === 'feature1';
-        const pred2 = b => b === 'feature2';
+    trie.setValue([pred1, pred2], 'pred1-pred2-value');
 
-        trie.setValue([pred1, pred2], 'pred1-pred2-value');
-        trie.setValue([pred1, pred2], 'new-pred1-pred2-value');
-
-        assert.equal(trie.getValue(['feature1', 'feature2']), 'new-pred1-pred2-value');
-    });
+    assert.equal(trie.getValue(['feature1']), undefined);
+    assert.equal(trie.getValue(['feature1', 'feature2', 'feature3']), undefined);
 });
+
+test('Getting a value results in undefined if the input features are not satisfied', function () {
+    const trie = new Trie();
+    const pred1 = a => true
+    const pred2 = b => false
+
+    trie.setValue([pred1, pred2], 'pred1-pred2-value');
+
+    assert.equal(trie.getValue(['feature1', 'feature2']), undefined);
+});
+
+test('Setting a value overrides previous values associated with the same predicates', function () {
+    const trie = new Trie();
+    const pred1 = a => a === 'feature1';
+    const pred2 = b => b === 'feature2';
+
+    trie.setValue([pred1, pred2], 'pred1-pred2-value');
+    trie.setValue([pred1, pred2], 'new-pred1-pred2-value');
+
+    assert.equal(trie.getValue(['feature1', 'feature2']), 'new-pred1-pred2-value');
+});
+
+test.run();
